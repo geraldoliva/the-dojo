12f96d0aab1466ca0b6b7118da029c973ae788cd
Gerald Oliva <geraldoliva@geralds-imac.myfiosgateway.com>
2021-11-27 18:43:17 -0500

    Add logging users in functionality

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 // styles
 import "./Login.css";
@@ -6,10 +7,11 @@ import "./Login.css";
 export default function Dashboard() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -33,14 +35,13 @@ export default function Dashboard() {
           value={password}
         />
       </label>
-      <button>Login</button>
-      {/*{!isPending && <button className="btn">Login</button>}
+      {!isPending && <button className="btn">Login</button>}
       {isPending && (
         <button className="btn" disabled>
           Loading
         </button>
       )}
-      {error && <div className="error">{error}</div>} */}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 }
