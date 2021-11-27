cc20f454a8ffdb121ba57f86f185c04b54471d92
Gerald Oliva <geraldoliva@geralds-imac.myfiosgateway.com>
2021-11-27 18:10:09 -0500

    -Add Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,46 @@
+import { useState } from "react";
+
 // styles
 import "./Login.css";
 
 export default function Dashboard() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button>Login</button>
+      {/*{!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>} */}
+    </form>
+  );
 }
