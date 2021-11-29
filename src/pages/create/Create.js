638593e489c830bde518391d0cf526e9a027f1f2
Gerald Oliva <geraldsamoliva@gmail.com>
2021-11-28 23:22:51 -0500

    Add functionality to save new projects

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -3,6 +3,8 @@ import Select from "react-select";
 import { useCollection } from "../../hooks/useCollection";
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useHistory } from "react-router";
 
 // styles
 import "./Create.css";
@@ -15,6 +17,9 @@ const categories = [
 ];
 
 export default function Create() {
+  const history = useHistory();
+  const { addDocument, response } = useFirestore("projects");
+
   const { documents } = useCollection("users");
   const [users, setUsers] = useState([]);
   const { user } = useAuthContext();
@@ -36,7 +41,7 @@ export default function Create() {
     }
   }, [documents]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setFormError(null);
 
@@ -73,7 +78,11 @@ export default function Create() {
       createdBy,
       assignedUsersList,
     };
-    console.log(project);
+
+    await addDocument(project);
+    if (!response.error) {
+      history.push("/");
+    }
   };
 
   return (
