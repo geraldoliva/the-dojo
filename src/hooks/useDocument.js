76443865d1f3cf545c01201a23f7e121fed8a362
Gerald Oliva <geraldsamoliva@gmail.com>
2021-12-01 19:37:53 -0500

    Add ProjectSummary component

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -11,8 +11,13 @@ export const useDocument = (collection, id) => {
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        }
+        else {
+          setError('no such document exists')
+        }
       },
       (err) => {
         console.log(err.message);
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,6 +1,25 @@
+import { useParams } from "react-router";
+import { useDocument } from "../../hooks/useDocument";
+import ProjectSummary from "./ProjectSummary";
+
 // styles
 import "./Project.css";
 
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <ProjectSummary project={document} />
+    </div>
+  );
 }
